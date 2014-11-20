e23cad28edf5bee7c2864ac0eb2407087ac34e72
Craig J Smith <nctiggy@gmail.com>
2014-11-19 18:59:10 -0800

    table improvements

--- app.js
+++ app.js
@@ -4,24 +4,12 @@
   var duns = [
     { 
       name: "Microsoft",
-      id: "81466849" 
+      id: "081466849" 
     },
     {
       name: "Costco",
       id: "103391843"
-    },
-		{
-			name: "Los Angeles International",
-			id: "LAX"
-		},
-		{
-			name: "Houston",
-			id: "IAH"
-		},
-		{
-			name: "Chicago",
-			id: "ORD"
-		}
+    }
   ];
 
   app.controller("MsInvController", function($scope, $filter, InventoryFactory) {
@@ -29,6 +17,7 @@
     $scope.getData = function() {
     	InventoryFactory.getInv($scope.selectedDuns).success(function(data) {
     		$scope.myData = data['rows'];
+    		//$scope.myPages = data['pages'];
 			})
 		};
 	});
@@ -37,7 +26,7 @@
 
     return {
       getInv: function(dunId){
-        return $http.get('http://opsconsole.corp.emc.com/sitelookup/new/models/getdataTLA.php?fld=Global%20Duns%20Number&val=' + dunId + '&_search=false&nd=1415402285504&rows=4000&page=1&sidx=&sord=asc&_=1415391480174')
+        return $http.get('http://opsconsole.corp.emc.com/sitelookup/new/models/getdataTLA.php?fld=Global%20Duns%20Number&val=' + dunId + '&_search=false&nd=1415402285504&rows=40&page=1&sidx=&sord=asc&_=1415391480174')
         //return $http.get('http://services.faa.gov/airport/status/' + air + '?format=application/json')
 			}
     }
